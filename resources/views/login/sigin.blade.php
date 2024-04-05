e5385432cc9eae0bac150b8185e31e11e9646d5f
trần đức thắng <tranducthang.uway.@gmail.com>
2024-04-05 17:31:04 +0700

    project

--- resources/views/login/sigin.blade.php
+++ resources/views/login/sigin.blade.php
@@ -13,24 +13,16 @@
 <div class="Headline">
 <div class="sigin-name">
 <h1 class="h text-Blue  f">MyTalent</h1>
-
 <p class="h">Nền tản tuyển dụng thông minh công nghệ Al</p>
 </div>
- 
-
 <div class="sigin-col-1">  
-
-
-    <div class="tap">       
-<div class="name-sig text-Fuchsia text-hover-Blue-light-2 ">Đăng Nhập</div>
-
-
-
-
-<div class="name-sig">Đăng Ký</div>
+<div class="tap">
+  <button class="name-sig text-Fuchsia text-hover-Blue-light-2 " onclick="openCity(event, 'Đăng ký')" id="defaultOpen">Đăng ký</button>
+  <button class="name-sig text-Fuchsia text-hover-Blue-light-2" onclick="openCity(event, 'Đăng nhập')">Đăng nhập</button>
 </div>
-    <div class="da"></div>
+<div class="da"></div>
 <!-- form sigin -->
+<div id="Đăng ký" class="tab-form">
 <form action="">
 <div class="from-col-email">
 <label>Email</label>
@@ -46,16 +38,17 @@
 </div>
 </div>
 <div class="tap-input">
-    <div class="name-check"> <input type="checkbox"> Ghi nhớ </div>
-    <div class="name-sig"> Quên mật khẩu</div>
-        </div>
+<div class="name-check"> <input type="checkbox"> Ghi nhớ </div>
+<div class="name-sig"> Quên mật khẩu</div>
+</div>
 <div class="from-col-submit  d">
 <input type="submit" class="btn " value="Submit">
 </div>
 </form>
-    </div>
+</div>
 </div>
 </section>    
 </header>
+
 </body>
 </html>
