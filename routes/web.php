83ee1aaaf77beec8a020b2ce829f1eb826858a56
trần đức thắng <tranducthang.uway.@gmail.com>
2024-04-18 19:30:40 +0700

    Fix error of modules running, error of missing transmission lines and missing module plugs

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\cv\addcv;
 use App\Http\Controllers\job\add_job;
@@ -9,14 +10,13 @@ use App\Http\Controllers\Auth\loginController;
 
 
 
-
 Route::prefix('login')->group(function(){
     Route::post('/login', [LoginController::class, 'login'])->name('login1');
     Route::post('/login1', [RegisterController::class, 'login1']);
-Route::get('/register', [RegisterController::class, 'showlogin'])->name('register');
-Route::post('/register',[RegisterController::class,'create']);
-Route::get('/forgetpass',[RegisterController::class,'quenpass']);
-Route::get('/newforgetpass',[RegisterController::class,'uploadpass']);
+    Route::get('/register', [RegisterController::class, 'showlogin'])->name('register');
+    Route::post('/register',[RegisterController::class,'create']);
+    Route::get('/forgetpass',[RegisterController::class,'quenpass']);
+    Route::get('/newforgetpass',[RegisterController::class,'uploadpass']);
 });
 Route::get('/verify-token', [RegisterController::class, 'showVerifyTokenForm'])->name('verify.token');
 Route::post('/verify-token', [RegisterController::class,'verifyToken'])->name('verify.token');
@@ -24,15 +24,10 @@ Route::post('/verify-token', [RegisterController::class,'verifyToken'])->name('v
 
 
 Route::prefix('cv')->group(function(){
- Route::get('/add',[addcv::class,'showaddcv'])->name('cv_job');
- Route::post('/add',[addcv::class,'cvJob'])->name('cv_job');
- Route::get('/listcv',[addcv::class,'showcv']);
-
-
+    Route::get('/add',[addcv::class,'showaddcv'])->name('cv_job');
+    Route::post('/add',[addcv::class,'cvJob'])->name('cv_job');
+    Route::get('/listcv',[addcv::class,'showcv']);
 });
-
-
-
 
 Route::prefix('js_job')->group(function(){
     Route::get('/job',[ add_job::class,'showadd_job'])->name('job');
@@ -41,12 +36,4 @@ Route::prefix('js_job')->group(function(){
     Route::get('/jobs/search', [add_job::class, 'filter'])->name('jobs.filter');
     Route::post('jobs.filter', [add_job::class, 'filter'])->name('jobs.filter');
     // Trong phương thức filter của controller
-
-
-   });  
-       
-    
-
-
-
-
+});  
