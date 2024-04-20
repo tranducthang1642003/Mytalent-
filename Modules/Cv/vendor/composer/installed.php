96f7eca8b7d9199ab76da34fab8083b0ec0cdb97
trần đức thắng <tranducthang.uway.@gmail.com>
2024-04-20 11:51:55 +0700

    sai

---
+++ Modules/Cv/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nwidart/cv',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '18214f6ef062591cf7045a551aa6ad091f4660b2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nwidart/cv' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '18214f6ef062591cf7045a551aa6ad091f4660b2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
